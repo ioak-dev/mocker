46cc9687e03c7ebe79cd01a2d91365a43659f440
Arun Kumar Selvaraj <dev@ioak.org>
2020-12-25 23:37:13 +0530

    Website layout changes, add capability for sidebar navigation

--- src/oakui/OakTable.test.tsx
+++ src/oakui/OakTable.test.tsx
@@ -8,10 +8,6 @@ describe('table component interaction', () => {
     const wrapper = shallow(<OakTable header={[]} data={[]} />);
     expect(wrapper.find('.oak-table')).toHaveLength(1);
   });
-  it('should show in material style when material is set as true', () => {
-    const wrapper = shallow(<OakTable header={[]} data={[]} material />);
-    expect(wrapper.find('.oak-table').hasClass('material')).toBe(true);
-  });
   it('should not show in material style when material is set as false', () => {
     const wrapper = shallow(<OakTable header={[]} data={[]} />);
     expect(wrapper.find('.oak-table').hasClass('material')).toBe(false);
