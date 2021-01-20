bd366daae2bb9d2fc0c68c23accf2211ad175355
Arun Kumar Selvaraj <dev@ioak.org>
2021-01-20 14:36:22 +0530

    Updates

---
+++ precache-manifest.c649947b5a09b2b2db56f96145ba50b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a51262951509ce17f7a33564658c968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98494ed98922ce88fe44",
+    "url": "/static/css/main.06f4fb96.chunk.css"
+  },
+  {
+    "revision": "bb7c2a5363167edcf777",
+    "url": "/static/js/2.c34a6474.chunk.js"
+  },
+  {
+    "revision": "c6add204db1feac3a9caaab2162d8b1e",
+    "url": "/static/js/2.c34a6474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98494ed98922ce88fe44",
+    "url": "/static/js/main.a5640ad4.chunk.js"
+  },
+  {
+    "revision": "bdebf0ca626f10b48817",
+    "url": "/static/js/runtime-main.454f82fd.js"
+  },
+  {
+    "revision": "bc9580be27db18160cd8075f5679f0d3",
+    "url": "/static/media/login_bg.bc9580be.jpg"
+  },
+  {
+    "revision": "fb9493fedf192e85a6deb1489fcea02d",
+    "url": "/static/media/mockback_black.fb9493fe.svg"
+  },
+  {
+    "revision": "182166ffac55b831e36395fe583d6e60",
+    "url": "/static/media/mockback_white.182166ff.svg"
+  }
+]);
